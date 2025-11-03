5e4b554e70473f970039c90f582874a13c7d6a21
Marcus Sanatan <msanatan@gmail.com>
2025-11-03 15:58:00 -0400

    It's time to let go, all dev for the plugin happens in MCPForUnity (#365)

--- UnityMcpBridge/UnityMcpServer~/src/conftest.py
+++
@@ -1,4 +0,0 @@
-def pytest_ignore_collect(path, config):
-    # Avoid duplicate import mismatches between Bridge and MCPForUnity copies
-    p = str(path)
-    return p.endswith("test_telemetry.py")
